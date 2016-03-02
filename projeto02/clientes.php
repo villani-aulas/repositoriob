bd3610defb5ffb34605fb581c89c1268a2abbd98
Leonardo Villani <informatica@villani.eti.br>
2016-03-02 12:28:54 -0300

    Projeto02 - Alteração do conteúdo das páginas cliente e sobre-nos.

--- projeto02/clientes.php
+++ projeto02/clientes.php
@@ -1,1 +1,15 @@
-<?php
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Página de Clientes</title>
+</head>
+<body>
+	<h1>Página de Clientes</h1>
+	<ul>
+		<li>Início</li>
+		<li><a href="sobre-nos.php">Sobre nós</a></li>
+		<li> > Clientes</li>
+	</ul>
+</body>
+</html>
--- projeto02/sobre-nos.php
+++ projeto02/sobre-nos.php
@@ -1,1 +1,15 @@
-<?php
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Sobre nós</title>
+</head>
+<body>
+	<h1>Sobre nós</h1>
+	<ul>
+		<li>Início</li>
+		<li> > Sobre nós</li>
+		<li> <a href="clientes.php">Clientes</a></li>
+	</ul>
+</body>
+</html>
